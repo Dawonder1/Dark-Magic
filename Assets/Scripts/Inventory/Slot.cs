40baddf758d6e2df9fb96614461ff6c829af4895
Dawonder1 <you@example.com>
2024-06-26 13:26:44 +0100

    working inventory

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -1,33 +1,31 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     public Item item;
     [SerializeField] Sprite itemImage;
-    TextMeshProUGUI numOwnedText;
-
-    private void OnEnable()
-    {
-        loadItem();
-        item.itemUsed += loadItem;
-    }
+    [SerializeField] TextMeshProUGUI numOwnedText;
 
     public void setItem(Item itemToSet)
     {
         item = itemToSet;
         loadItem();
+        item.itemUsed += loadItem;
     }
 
     void loadItem()
     {
         itemImage = item.sprite;
-        GetComponent<SpriteRenderer>().sprite = itemImage;
+        GetComponent<Image>().sprite = itemImage;
+        if(item == null) { return; }
         numOwnedText.text = item.numOwned.ToString();
     }
 
     private void OnDisable()
     {
+        if (item == null) return;
         item.itemUsed -= loadItem;
     }
 }
